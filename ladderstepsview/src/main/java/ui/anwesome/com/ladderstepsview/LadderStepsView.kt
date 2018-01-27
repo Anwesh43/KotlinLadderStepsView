5dcac41b8db55cfe0eae3aed740c3067a724131d
Anwesh43 <anweshthecool0@gmail.com>
2018-01-27 07:41:20 +0530

    adding LadderStepsView

--- ladderstepsview/src/main/java/ui/anwesome/com/ladderstepsview/LadderStepsView.kt
+++ ladderstepsview/src/main/java/ui/anwesome/com/ladderstepsview/LadderStepsView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.ladderstepsview
 /**
  * Created by anweshmishra on 27/01/18.
  */
+import android.graphics.*
+import android.content.*
+import android.view.*
+class LadderStepsView(ctx:Context):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
